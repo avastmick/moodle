7902845ed2a971b2e5d64c6ff2156809f126c3f0
avastmick <avastmick@outlook.com>
2017-08-29 16:29:35 +0800

    Base install setup and initial doco

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-A docker instance to run Moodle on a VPS
+# A Docker instance to run Moodle on a VPS
+
+# TL;DR;
+
+```bash
+> git clone https://github.com/avastmick/moodle.git
+> cd moodle
+> sudo setup.sh
+```
+
+# Installation
+
+This uses the [Bitnami Docker Moodle](https://github.com/bitnami/bitnami-docker-moodle) images.
+
+## Configuration
+
+There are defaults for all configuration.
+
+Specific configuration is done via the ``.conf`` file in the project root.
+
+TODO: Configure the SMTP server and local mailserver
+TODO: Configure the domain settings
+TODO: Security
+TODO: HTTPS and certs
+
+## Note on installations in China
+
+### Installing Docker
+
+Expected: usage of Ubuntu server.
+
+The following configuration using the local mirrors for updates works.
+
+Edit: ``sudo nano /etc/apt/sources.list``
+
+```bash
+deb http://cn.archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse
+deb http://cn.archive.ubuntu.com/ubuntu/ xenial-updates main restricted universe multiverse
+deb http://security.ubuntu.com/ubuntu xenial-security main restricted universe multiverse
+
+# Docker
+deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu xenial edge
+deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial edge
+```
+
+### Pulling images
+
+Add the following /etc/docker/daemon.json to pull from the China registry mirror by default. (restart docker daemon)
+
+```json
+{
+  "registry-mirrors": ["https://registry.docker-cn.com"]
+}
+```
+
--- setup.sh
+++ setup.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
 
-
-
 ###########################################################
 #
 # This script sets up the VPS host to install a containerized
 #   instance of Moodle
 #
+# Run as root (or sudo ./setup.sh)
+#
 # - Pre-requisites:
+# 0. Ubuntu Server 16.04.3+
 # 1. Installed Git
 #
 # - This script runs the following:
 # 1. Install Docker
-# 2. Install docker-compose
+# 2. Install docker-compose (https://docs.docker.com/compose/install/)
 # 3. Setup email server
-# 4. Setup image persistence
+# 4. Setup image persistence on host (included in docker-compose)
 # 5. Install Moodle images via docker-compose
 #
 ###########################################################
 
-# 0. Update repositories and packages
-sudo apt-get update -y
+DOCKER-COMPOSE-VER=1.15.0
 
 # 1. Install Docker
-command -v docker >/dev/null && { echo docker installed;  } || { echo "docker not found, installing."; sudo curl -sSL https://get.docker.com/ | sh; sudo chkconfig docker on; sudo service docker start;};
+command -v docker >/dev/null && { echo docker installed;  } || { echo "docker not found, installing."; curl -sSL https://get.docker.com/ | sh ;};
 
-# 2. Check that docker-compose runs
-command -v docker-compose >/dev/null && { echo docker-compose installed;  } || { echo "docker-compose not found, installing.";};
+# 2. Install docker-compose
+command -v docker-compose >/dev/null && { echo docker-compose installed;  } || { echo "docker-compose not found, installing."; curl -L https://github.com/docker/compose/releases/download/$DOCKER-COMPOSE-VER/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose; chmod +x /usr/local/bin/docker-compose; };
 
-# 3. Setup email
+# 3. Setup email server
 
-# 4. Setup image persistence
-
-# 5. Install Moodle
+# 4. Install Moodle
+docker-compose up -d
